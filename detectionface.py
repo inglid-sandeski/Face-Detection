4f5da570aa65d07478865a656aea310409b14f95
Inglid Sandeski <bynnesandeski@gmail.com>
2024-06-24 02:50:22 -0400

    Update detectionface.py

--- detectionface.py
+++ detectionface.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 classificador = cv2.CascadeClassifier('.venv\\Scripts\\haarcascade_frontalface_default.xml')
-imagem = cv2.imread('.venv\\beatles.jpg')
+imagem = cv2.imread('.venv\\WhatsApp Image 2024-06-24 at 02.41.43.jpeg')
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 facesDetectadas = classificador.detectMultiScale(imagemCinza)
 print(len(facesDetectadas))
